e153a70c6e0ddb67136925969e93238f6b713479
petiteranjalia <167243616+petiteranjalia@users.noreply.github.com>
2024-09-12 13:54:55 +0300

    thedemsindex.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["MANAGE SERVERS","BOT ASSIST BY THE'DEMS"];
 
 
 let currentIndex = 0;
